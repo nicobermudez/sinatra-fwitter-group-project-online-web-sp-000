d6f6adb8ae60148b624c817d913a3a0731e812ff
Nicolas Bermudez <bermnicolas@gmail.com>
2019-04-29 22:14:15 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,18 +8,18 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
-  erb :index
-end
-
-helpers do
-  def logged_in?
-    !!current_user
+    erb :index
   end
 
-  def current_user
-    @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
-  end
+  helpers do
+    def logged_in?
+      !!current_user
+    end
 
-end
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id]) if session[:user_id]
+    end
+
+  end
 
 end
